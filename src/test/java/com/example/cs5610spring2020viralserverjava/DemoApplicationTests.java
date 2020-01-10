abdb4f36aad21790e7caf1cf4868fb79548347f9
viralbpatel <patel.viral@husky.neu.edu>
2020-01-10 10:21:27 -0500

    Initial commit with server setup

---
+++ src/test/java/com/example/cs5610spring2020viralserverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs5610spring2020viralserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
