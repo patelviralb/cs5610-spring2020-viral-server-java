e8c38e2876fc1aa41dbbdd32733a65182e21632f
viralbpatel <patel.viral@husky.neu.edu>
2020-01-26 21:44:48 -0500

    user.admin.controller completed

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -1,63 +1,111 @@
 (function () {
-  /*var $usernameFld, $passwordFld;
-  var $removeBtn, $editBtn, $createBtn;
-  var $firstNameFld, $lastNameFld, $roleFld;
-  var $userRowTemplate, $tbody;
-  var userService = new AdminUserServiceClient();
-  $(main);*/
-
-  /*function main() { … }
-  DONE//function createUser() { … }
-  DONE//function findAllUsers() { … }
-  function findUserById() { … }
-  DONE//function deleteUser() { … }
-  function selectUser() { … }
-  function updateUser() { … }
-  DONE//function renderUser(user) { … }
-  DONE//function renderUsers(users) { … }*/
-
   let userService = new AdminUserServiceClient();
   let $usernameFld, $passwordFld, $firstNameFld, $lastNameFld, $roleFld;
   let $addUserBtn, $updateUserBtn;
   let $tableBody, $rowTemplate;
 
+  /**
+   * Functions required as per document.
+   */
   const main = () => {
     $tableBody = $('tbody');
     $rowTemplate = $('.vp-cs5610-row-template');
+
+    $usernameFld = $('#usernameFld');
+    $passwordFld = $('#passwordFld');
+    $firstNameFld = $('#firstNameFld');
+    $lastNameFld = $('#lastNameFld');
+    $roleFld = $('#roleFld');
     $addUserBtn = $('#addUserBtn');
     $updateUserBtn = $('#updateUserBtn');
 
     findAllUsers();
 
     $addUserBtn.click(createUser);
+  };
 
-    /**/
-    /*var testUser = {
-      "username"  : "Edited User",
-      "password"  : "Password",
-      "firstName" : "Edited First Name",
-      "lastName" : "Edited Last Name",
-      "role" : "Edited Role"
-    };
-    userService.updateUser("FCbR5uj2FXT63Y3Z", testUser).then(() => {
-      alert("EDITED");
-    });*/
-    /**/
+  const createUser = () => {
+    let user = makeUserData();
+    userService.createUser(user).then(renderUser);
+
+    clearAllFields();
   };
 
   const findAllUsers = () => {
     userService.findAllUsers().then(renderUsers);
   };
 
+  const findUserById = (userID) => {
+    userService.findUserById(userID).then(selectUser);
+  };
 
+  const deleteUser = (userID) => {
+    userService.deleteUser(userID).then(() => removeRow(userID))
+  };
 
-  const createUser = () => {
-    $usernameFld = $('#usernameFld');
-    $passwordFld = $('#passwordFld');
-    $firstNameFld = $('#firstNameFld');
-    $lastNameFld = $('#lastNameFld');
-    $roleFld = $('#roleFld');
+  const selectUser = (userData) => {
+    $usernameFld.val(userData.username);
+    $passwordFld.val(userData.password);
+    $firstNameFld.val(userData.firstName);
+    $lastNameFld.val(userData.lastName);
+    $roleFld.val(userData.role);
 
+    $updateUserBtn.click(() => updateUser(userData));
+
+    $updateUserBtn.attr('disabled', false);
+    $updateUserBtn.removeClass("btn-secondary");
+    $updateUserBtn.addClass("btn-success");
+
+    $addUserBtn.attr('disabled', true);
+    $addUserBtn.removeClass("btn-success");
+    $addUserBtn.addClass("btn-secondary");
+  };
+
+  const updateUser = (userData) => {
+    let user = makeUserData();
+    userService.updateUser(userData._id, user).then(() => {
+      userService.findUserById(userData._id).then(renderUpdatedUser);
+    });
+
+    $updateUserBtn.attr('disabled', true);
+    $updateUserBtn.addClass("btn-secondary");
+    $updateUserBtn.removeClass("btn-success");
+    $updateUserBtn.unbind("click");
+
+    $addUserBtn.attr('disabled', false);
+    $addUserBtn.addClass("btn-success");
+    $addUserBtn.removeClass("btn-secondary");
+  };
+
+  const renderUser = (user) => {
+    const rowToClone = $rowTemplate.clone();
+
+    rowToClone.removeClass('vp-cs5610-row-template-hidden');
+    rowToClone.attr('id',"userRow_" + user._id);
+    rowToClone.find('.vp-cs5610-template-username').html(user.username);
+    rowToClone.find('.vp-cs5610-template-password').html(user.password);
+    rowToClone.find('.vp-cs5610-template-firstName').html(user.firstName);
+    rowToClone.find('.vp-cs5610-template-lastName').html(user.lastName);
+    rowToClone.find('.vp-cs5610-template-role').html(user.role);
+    rowToClone.find('.vp-cs5610-template-delete-btn').click(() => deleteUser(user._id));
+    rowToClone.find('.vp-cs5610-template-edit-btn').click(() => findUserById(user._id));
+    $tableBody.prepend(rowToClone);
+  };
+
+  const renderUsers = (allUsers) => {
+    $tableBody.empty();
+
+    for(var userIndex in allUsers) {
+
+      const eachUser = allUsers[userIndex];
+      renderUser(eachUser);
+    }
+  };
+
+  /**
+   * Helper function
+   */
+  const makeUserData = () => {
     let username = $usernameFld.val();
     let password = "####Encrypted####";
     let firstName = $firstNameFld.val();
@@ -72,60 +120,29 @@
       "role" : role
     };
 
-    userService.createUser(user).then(renderUser);
+    return user;
+  };
 
+  const removeRow = (userID) => {
+    let $elementToRemove = $(`#userRow_${userID}`);
+    $elementToRemove.remove();
+
+    clearAllFields();
+  };
+
+  const renderUpdatedUser = (userData) => {
+    let elementToUpdate = $(`#userRow_${userData._id}`);
+    elementToUpdate.remove();
+    renderUser(userData);
+    clearAllFields();
+  };
+
+  const clearAllFields = () => {
     $usernameFld.val("");
     $passwordFld.val("");
     $firstNameFld.val("");
     $lastNameFld.val("");
     $roleFld.prop('selectedIndex',0);
-  };
-
-  const renderUsers = (allUsers) => {
-    $tableBody.empty();
-
-    for(var userIndex in allUsers) {
-
-      const eachUser = allUsers[userIndex];
-      renderUser(eachUser);
-      /*const rowToClone = $rowTemplate.clone();
-
-      rowToClone.removeClass('vp-cs5610-row-template-hidden');
-      rowToClone.attr('id',eachUser._id);
-      rowToClone.find('.vp-cs5610-template-username').html(eachUser.username);
-      rowToClone.find('.vp-cs5610-template-password').html(eachUser.password);
-      rowToClone.find('.vp-cs5610-template-firstName').html(eachUser.firstName);
-      rowToClone.find('.vp-cs5610-template-lastName').html(eachUser.lastName);
-      rowToClone.find('.vp-cs5610-template-role').html(eachUser.role);
-      rowToClone.find('.vp-cs5610-template-delete-btn').click(() => deleteUser(eachUser._id));
-      /!*rowToClone.find('.vp-cs5610-template-edit-btn').click(() => #(eachUser._id));*!/
-      $tableBody.append(rowToClone);*/
-    }
-  };
-
-  const deleteUser = (userID) => {
-    userService.deleteUser(userID).then(() => removeRow(userID))
-  };
-
-  const removeRow = (userID) => {
-    let $elementToRemove = $(`#deleteBtn_${userID}`);
-    $elementToRemove.remove();
-  };
-
-  const renderUser = (user) => {
-    console.log(user);
-    const rowToClone = $rowTemplate.clone();
-
-    rowToClone.removeClass('vp-cs5610-row-template-hidden');
-    rowToClone.attr('id',"deleteBtn_" + user._id);
-    rowToClone.find('.vp-cs5610-template-username').html(user.username);
-    rowToClone.find('.vp-cs5610-template-password').html(user.password);
-    rowToClone.find('.vp-cs5610-template-firstName').html(user.firstName);
-    rowToClone.find('.vp-cs5610-template-lastName').html(user.lastName);
-    rowToClone.find('.vp-cs5610-template-role').html(user.role);
-    rowToClone.find('.vp-cs5610-template-delete-btn').click(() => deleteUser(user._id));
-    /*rowToClone.find('.vp-cs5610-template-edit-btn').click(() => #(user._id));*/
-    $tableBody.prepend(rowToClone);
   };
 
   $(main);
