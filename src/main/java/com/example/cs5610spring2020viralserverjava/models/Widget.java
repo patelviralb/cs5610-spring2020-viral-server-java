a75304007e62ad76e38672a072c700ddbedd41b6
viralbpatel <patel.viral@husky.neu.edu>
2020-02-16 18:52:11 -0500

    Added widget model

---
+++ src/main/java/com/example/cs5610spring2020viralserverjava/models/Widget.java
@@ -0,0 +1,100 @@
+package com.example.cs5610spring2020viralserverjava.models;
+
+public class Widget {
+  private String name;
+  private String id;
+  private String type;
+  private int order;
+  private String text;
+  private String source;
+  private int size;
+  private int width;
+  private int height;
+  private String cssClass;
+  private String style;
+  private String value;
+
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getId() {
+    return id;
+  }
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getType() {
+    return type;
+  }
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public int getOrder() {
+    return order;
+  }
+  public void setOrder(int order) {
+    this.order = order;
+  }
+
+  public String getText() {
+    return text;
+  }
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  public String getSource() {
+    return source;
+  }
+  public void setSource(String source) {
+    this.source = source;
+  }
+
+  public int getSize() {
+    return size;
+  }
+  public void setSize(int size) {
+    this.size = size;
+  }
+
+  public int getWidth() {
+    return width;
+  }
+  public void setWidth(int width) {
+    this.width = width;
+  }
+
+  public int getHeight() {
+    return height;
+  }
+  public void setHeight(int height) {
+    this.height = height;
+  }
+
+  public String getCssClass() {
+    return cssClass;
+  }
+  public void setCssClass(String cssClass) {
+    this.cssClass = cssClass;
+  }
+
+  public String getStyle() {
+    return style;
+  }
+  public void setStyle(String style) {
+    this.style = style;
+  }
+
+  public String getValue() {
+    return value;
+  }
+  public void setValue(String value) {
+    this.value = value;
+  }
+}
